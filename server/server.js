d97c5d0731f6a2121ca204dbb754a54d1b2d08d1
mantast255 <mantast255@gmail.com>
2018-10-18 10:39:46 +0300

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -42,14 +42,21 @@ io.on('connection', (socket) => { //register event listener;
   });
 
   socket.on('createMessage', (newMessage, callback) => {
-    console.log('CreatedMessage', newMessage);
-    io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+    var user = users.getUser(socket.id);
+
+    if(user && isRealString(newMessage.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, newMessage.text));
+    }
     callback();
   })
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-  })
+    var user = users.getUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
+  });
 
   socket.on('disconnect', () => {
     var user = users.removeUser(socket.id);
